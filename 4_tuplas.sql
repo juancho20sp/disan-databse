6f4078628ce16d4e85499f0122fb8f96be4f1e3e
Laura Garcia <laura.garcia-l@outlook.com>
2021-04-10 21:55:17 -0500

    Restricciones de Tupla

---
+++ 4_tuplas.sql
@@ -0,0 +1,41 @@
+ALTER TABLE Battalion 
+	ADD CONSTRAINT CK_BATTALION_MILFORCE 
+	CHECK (militaryForce IN ('Fuerza Aérea', 'Armada', 'Ejército'));
+	
+ALTER TABLE Brigade 
+	ADD CONSTRAINT CK_BRIGADE_MILFORCE
+	CHECK (militaryForce IN ('Fuerza Aérea', 'Armada', 'Ejército'));
+	
+ALTER TABLE Division 
+	ADD CONSTRAINT CK_DIVISION_MILFORCE
+	CHECK (militaryForce IN ('Fuerza Aérea', 'Armada', 'Ejército'));	
+	
+ALTER TABLE Doctor 
+	ADD CONSTRAINT CK_DOCTOR_MILFORCE
+	CHECK (militaryForce IN ('Fuerza Aérea', 'Armada', 'Ejército'));	
+
+ALTER TABLE Nurse 
+	ADD CONSTRAINT CK_NURSE_MILFORCE
+	CHECK (militaryForce IN ('Fuerza Aérea', 'Armada', 'Ejército'));
+	
+ALTER TABLE Person 
+	ADD CONSTRAINT CK_PERSON_DOCTYPE
+	CHECK (documentType IN ('CC', 'CE', 'TI', 'RC', 'PS'));
+	
+ALTER TABLE Person 
+	ADD CONSTRAINT CK_PERSON_GENDER
+	CHECK (documentType IN ('F', 'M'));
+	
+ALTER TABLE Person 
+	ADD CONSTRAINT CK_PERSON_STATUS
+	CHECK (documentType IN ('A', 'I'));
+	
+ALTER TABLE Person 
+	ADD CONSTRAINT CK_PERSON_EMAIL 
+	CHECK (REGEXP_LIKE(email, '^[A-Za-z]*[0-9]*@[A-Za-z]*.[A-Za-z]*$'));
+	
+ALTER TABLE Person 
+	ADD CONSTRAINT CK_PERSON_EMAIL 
+	CHECK (REGEXP_LIKE(email, '^[A-Za-z]*[0-9]*@[A-Za-z]*.[A-Za-z]*$'));
+	
+	
